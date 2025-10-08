dd351e6b90a196ccd48c83e8865005fbd697165c
Sacchi4649 <sanyadika@gmail.com>
2025-10-09 00:09:57 +0700

    feat(Go Modules): Go modules section done

--- go-modules/app-say-hello/main.go
+++ go-modules/app-say-hello/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	say_hello_app "github.com/Sacchi4649/say-hello-app"
+	say_hello_app "github.com/Sacchi4649/say-hello-app/v2"
 )
 
 func main() {
-	fmt.Println(say_hello_app.SayHello())
+	fmt.Println(say_hello_app.SayHello("Sany"))
 }
